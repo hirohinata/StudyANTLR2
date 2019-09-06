c383946b751415759d526b165a06b51e03335ccc
hirohinata <azuri_hirohinata@hotmail.co.jp>
2019-09-07 01:14:16 +0900

    Transpilerプロジェクトの追加

---
+++ Transpiler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Transpiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "A := B;";
+            var expect = input;
+            var actual = StructuredTextToStructuredText(input);
+            Debug.Assert(expect == actual);
+        }
+
+        static string StructuredTextToStructuredText(string input)
+        {
+            return input;
+        }
+    }
+}
